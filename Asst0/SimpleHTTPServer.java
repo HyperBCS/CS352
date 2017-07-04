347c73aa7fed570e991cea992371e8adcceca44b
BuildTools <atomicbcs@gmail.com>
2017-07-04 14:35:16 -0400

    added return stuff

--- Asst0/SimpleHTTPServer.java
+++ Asst0/SimpleHTTPServer.java
@@ -3,44 +3,48 @@ import java.net.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 
 
-public class SimpleHTTPServer{
+public class SimpleHTTPServer {
 
 	ServerSocket serverSocket;
 	int port;
 
-	SimpleHTTPServer(String[] args){
+	SimpleHTTPServer(String[] args) {
 
 		// Initialize a serverSocket to accept clients
-		try{
+		try {
 			this.port = Integer.parseInt(args[0]);
 			this.serverSocket = new ServerSocket(port);
 
-		} catch(Exception e){
-			System.out.println(e);
+		} catch (Exception e) {
+			e.printStackTrace();
 			return;
 		}
 
 		Socket client = null;
-		while(true){
-			try{
+		while (true) {
+			try {
 				// Accept the client
 				client = serverSocket.accept();
+				client.setSoTimeout(3000);
 				HTTPThread clientThread = new HTTPThread(client);
 				Thread t = new Thread(clientThread);
 				t.start();
-			} catch(Exception e){
+			} catch (Exception e) {
 				// When we catch the error, print it out
 				System.out.println(e);
 			}
 		}
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// Check that our arguments are correct. If not we print message and exit.
-		if(args.length != 1){
+		if (args.length != 1) {
 			System.out.println("Usage: java -cp . SimpleHTTPServer 3456");
 			return;
 		}
@@ -48,14 +52,15 @@ public class SimpleHTTPServer{
 		return;
 	}
 
-	class HTTPThread implements Runnable{
+	class HTTPThread implements Runnable {
 		Socket clientSocket;
+		String reqStr;
 
-		HTTPThread(Socket c){
+		HTTPThread(Socket c) {
 			this.clientSocket = c;
 		}
 
-		private String logBuilder(Socket clientSocket, String req){
+		private String logBuilder(int status) {
 			StringBuilder pre = new StringBuilder();
 			SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 			Date date = new Date();
@@ -63,159 +68,202 @@ public class SimpleHTTPServer{
 			String addr = clientSocket.getInetAddress().getHostName();
 			int port = clientSocket.getPort();
 			pre.append(addr + ":" + port + " - ");
-			pre.append(req);
+			pre.append(reqStr + " - ");
+			pre.append(status);
 			return pre.toString();
 		}
 
 		/**
 		*This method will parse a request string and return a request object
 		**/
-		private ReqObj parseReq(String req){
+		private ReqObj parseReq(String req) {
 			String method = "";
 			String workingDir = ""; //path of root directory
 			String relativePath = ""; //user supplied path
 			String fullPath = ""; //merge root dir with relative path
-
 			ReqObj request = null; //to be returned
 
-			if(req.contains(" ")){
-				if(((req.length() - req.replaceAll(" ", "")) > 1) || req.charAt(req.indexOf(" ") + 1) != '/' || req.matches("//+")){
-					String bad = "bad request"
-					returnResponse(400, bad.getBytes());
-				} else{
-					method = req.substring(0, req.indexOf(" "));
-					relativePath = req.substring(req.indexOf(" ") + 1, req.length());
-					workinDir = System.getProperty("user.dir");
-					fullPath = workingDir + relativePath;
-					request = new ReqObj(method, fullPath);
-				}
+			String[] reqArr = req.split(" ");
+			if (reqArr.length < 2 || reqArr[0].length() < 3) {
+
+				String bad = "bad request";
+				returnResponse(400, bad.getBytes());
+				return null;
+			} else {
+				method = reqArr[0];
 			}
-			return request;
+			if (reqArr[1].length() > 1 && reqArr[1].charAt(0) == '/') {
+				try {
+					relativePath = java.net.URLDecoder.decode(reqArr[1], "UTF-8");
+					workingDir = System.getProperty("user.dir");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				fullPath = workingDir + relativePath;
+			}  else {
+				String bad = "bad request";
+				returnResponse(400, bad.getBytes());
+				return null;
+			}
+			return new ReqObj(method, fullPath);
+		}
+
+		public void doMethod(ReqObj request) {
+			String method = request.getMethod();
+			String fullPath = request.getResource();
+			String notImpl = "Not Implemented";
+			switch (method) {
+			case "GET":
+				request.setMethod("GET");
+				doGet(request);
+				break;
+			case "HEAD":
+				request.setMethod("HEAD");
+				returnResponse(501, notImpl.getBytes());
+				break;
+			case "POST":
+				request.setMethod("GET");
+				returnResponse(501, notImpl.getBytes());
+				break;
+			case "PUT":
+				request.setMethod("HEAD");
+				returnResponse(501, notImpl.getBytes());
+				break;
+			case "DELETE":
+				request.setMethod("GET");
+				returnResponse(501, notImpl.getBytes());
+				break;
+			default:
+				String bad = "Bad Request";
+				returnResponse(400, bad.getBytes());
+				break;
+			}
 		}
 
 		/**
 		*This method will perform get a resource defined in the ReqObj. Then it will call return response with the correct status and content.
 		* If there is an error in fetching the resource give null content, and the correct code.
 		**/
-		private void doGet(ReqObj req){
-				File file = new File(req.resource);
-				if(file.exists() && !file.isDirectory()){
-					if(file.canRead()){ //file is readable
-							InputStream inputStream = new FileInputStream(req.resource);
-							BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-							String str = bufferedReader.readLine();
-							StringBuilder stringBuilder = new StringBuilder();
-
-							while(str != null){
-								stringBuilder.append(str).append("\n");
-								str = bufferedReader.readLine();
-							}
-
-							String s = stringBuilder.toString(); //store contents of the file as text in a String
-							byte[] contents = s.getBytes();
-							returnResponse(200, contents);
-					} else{
-						String notReadable = "File not readable";
-						returnResponse(400, notReadable.getBytes());
+		private void doGet(ReqObj req) {
+			String filePath = req.getResource();
+			File file = new File(filePath);
+			if (file.exists() && !file.isDirectory()) {
+				if (file.canRead()) { //file is readable
+					try {
+						Path path = Paths.get(filePath);
+						byte[] contents = Files.readAllBytes(path);
+						returnResponse(200, contents);
+					} catch (Exception e) {
+						// need 500 error here
+						e.printStackTrace();
 					}
-				} else{
-						String fourOFour = "File not found";
-						returnResponse(404, fourOFour.getBytes());
+				} else {
+					String notReadable = "Internal Server Error";
+					returnResponse(500, notReadable.getBytes());
 				}
+			} else {
+				String fourOFour = "File not found";
+				returnResponse(404, fourOFour.getBytes());
+			}
 
 		}
 
 		/**
 		*This method will take a status, and content to be returned to the user.
 		**/
-		private void returnResponse(int status, byte[] content){
-			try(PrintStream pstream = new PrintStream(clientSocket.getOutputStream())){
-			HttpCodes codes = new HttpCodes();
-			pstream.println(codes.toString(status));
-			pstream.write(content);
-			} catch(Exception e){
-				System.out.println(e);
+		private void returnResponse(int status, byte[] content) {
+			// Prints the request along with other info about client
+			System.out.println(logBuilder(status));
+			try(PrintStream pstream = new PrintStream(clientSocket.getOutputStream())) {
+				HttpCodes codes = new HttpCodes();
+				pstream.println(codes.toString(status));
+				pstream.write(content);
+				pstream.flush();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 
 		}
 
 		@Override
-		public void run(){
+		public void run() {
 			try(
-				Socket client = clientSocket;
-		    	BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				){
-			// Reads the request from the client
-           	String s = in.readLine();
-           	// Prints the request along with other info about client
-			System.out.println(logBuilder(clientSocket, s));
-			// Parse the request, we can do a switch case based on request
-			ReqObj req = parseReq(s);
-
-			// THis is just to test sending a status and content to responder.
-			String respStr = "hello ram";
-			returnResponse(200,respStr.getBytes());
-			Thread.sleep(250);
-			} catch(Exception e){
-				System.out.println(e);
+				    Socket client = clientSocket;
+				    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				) {
+				// Reads the request from the client
+				try {
+					reqStr = in.readLine();
+				} catch (SocketTimeoutException e) {
+					returnResponse(408, "Request Timeout".getBytes());
+					return;
+				}
+				// Parse the request, we can do a switch case based on request
+				ReqObj req = parseReq(reqStr);
+				if (req != null) {
+					doMethod(req);
+				}
+				Thread.sleep(250);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 	}
 
-	class ReqObj{
-		String httpMethod;
-		String resource;
-		ReqObj(String httpMethod, String resource){
+	class ReqObj {
+		private String httpMethod;
+		private String resource;
+		ReqObj(String httpMethod, String resource) {
 			this.httpMethod = httpMethod;
 			this.resource = resource;
 		}
 
-		public String getMethod(){
+		public String getMethod() {
 			return this.httpMethod;
 		}
 
-		public void setMethod(String httpMethod){
+		public void setMethod(String httpMethod) {
 			this.httpMethod = httpMethod;
 		}
 
-		public String getResource(){
+		public String getResource() {
 			return this.resource;
 		}
 
-		public void setResource(String resource){
+		public void setResource(String resource) {
 			this.resource = resource;
 		}
 	}
 
 	public class HttpCodes {
 
-	    public HttpCodes() {
-	    }
+		public HttpCodes() {
+		}
 
-	    public String toString(int status) {
-	        switch (status) {
-	            case 200:
-	                return "HTTP/0.8 200 OK\r\n";
+		public String toString(int status) {
+			switch (status) {
+			case 200:
+				return "HTTP/0.8 200 OK\r\n";
 
-	            case 400:
-	                return "HTTP/0.8 200 Bad Request\r\n";
+			case 400:
+				return "HTTP/0.8 400 Bad Request\r\n";
 
-	            case 501:
-	                return "HTTP/0.8 501 Not Implemented\r\n";
+			case 501:
+				return "HTTP/0.8 501 Not Implemented\r\n";
 
-	            case 500:
-	                return "HTTP/0.8 500 Internal Server Error\r\n";
+			case 500:
+				return "HTTP/0.8 500 Internal Server Error\r\n";
 
-	            case 404:
-	                return "HTTP/0.8 404 Not Found\r\n";
+			case 404:
+				return "HTTP/0.8 404 Not Found\r\n";
 
-	            case 408:
-	                return "HTTP/0.8 408 Request Timeout\r\n";
+			case 408:
+				return "HTTP/0.8 408 Request Timeout\r\n";
 
-	            default:
-	                return "HTTP/0.8 200 Request Timeout\r\n";
-	        }
-	    }
+			default:
+				return "HTTP/0.8 200 OK\r\n";
+			}
+		}
 	}
 }
